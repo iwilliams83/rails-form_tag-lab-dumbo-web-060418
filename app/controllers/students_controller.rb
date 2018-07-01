d3a6c4d44bb7685c40dc64113c845d9ee6c7d1aa
iwilliams83 <indirajamurath@gmail.com>
2018-07-01 12:19:28 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
-  
+
+  def new
+    @student = Student.new
+    render :new
+  end
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(params[:student])
+    @student.save
   end
 
 end
