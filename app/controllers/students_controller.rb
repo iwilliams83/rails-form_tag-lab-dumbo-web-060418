6656183c29c6101cab40728c79af48087dbff569
iwilliams83 <indirajamurath@gmail.com>
2018-07-01 13:20:35 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
-    #redirect_to student_path
+    redirect_to students_path
   end
 
   def create
